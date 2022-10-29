9797cfe80f8cffb9cd5fb79156f7bc7720ee2a51
pipe <pipe@gmail.com>
2022-10-29 16:37:31 -0500

    comentario

--- modelos/AbstractModelo.py
+++ modelos/AbstractModelo.py
@@ -4,3 +4,7 @@ class AbstractModelo(metaclass=ABCMeta):
     def __init__(self,data):
         for llave, valor in data.items():
             setattr(self, llave, valor)
+            
+            
+            
+#otro comentario en el interface
